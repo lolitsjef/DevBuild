8d50110004172cccbccc1b1bb1956cbd5365def8
lolitsjef <lolitsjef@gmail.com>
2024-05-18 13:20:23 -0600

    container

--- assets/test-js.js
+++ assets/test-js.js
@@ -1,16 +1,17 @@
 
-var section1Items = document.getElementsByClassName("section1");
+var section1Items =document.getElementById('section1').getElementsByTagName('productcard');  //get all divs from div called container
 
 function selected(item) { //also pass in product here
-    //this.removeSelections();
+    this.removeSelections();
     item.style.backgroundColor = 'green';
 
-    item.classList.add('selected');
+    //item.classList.add('selected');
 }
 
 function removeSelections() {
     for(var i=0; i < section1Items.length; i++) {
         var item = section1Items[i];
-        item.classList.remove('selected');
+        item.style.backgroundColor = 'red';
+        //item.classList.remove('selected');
     }
 }
