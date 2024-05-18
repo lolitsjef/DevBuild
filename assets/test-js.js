63719cd0346067e096a5840f1af45e2e42b70d8c
lolitsjef <lolitsjef@gmail.com>
2024-05-18 13:13:22 -0600

    js

--- assets/test-js.js
+++ assets/test-js.js
@@ -2,11 +2,11 @@
 var section1Items = document.getElementsByClassName("section1");
 
 function selected(item) { //also pass in product here
-    this.clear();
+    //this.removeSelections();
     item.classList.add('selected');
 }
 
-function clear() {
+function removeSelections() {
     for(var i=0; i < section1Items.length; i++) {
         var item = section1Items[i];
         item.classList.remove('selected');
