9f9b849751d6474858bcf03c725751c026c8a4fb
lolitsjef <lolitsjef@gmail.com>
2024-05-18 13:38:11 -0600

    gpt

--- assets/test-js.js
+++ assets/test-js.js
@@ -1,14 +1,12 @@
-
-var section1Items = document.getElementById('section1').getElementsByClassName('productcard');  //get all divs from div called container
-
-function selected(item) { //also pass in product here
-    this.removeSelections();
+function selected(sectionIndex, item) {
+    removeSelections(sectionIndex);
     item.classList.add('selected');
 }
 
-function removeSelections() {
-    for(var i=0; i < section1Items.length; i++) {
-        var item = section1Items[i];
+function removeSelections(sectionIndex) {
+    var sectionItems = document.getElementById('section' + sectionIndex).getElementsByClassName('productcard');
+    for (var i = 0; i < sectionItems.length; i++) {
+        var item = sectionItems[i];
         item.classList.remove('selected');
     }
 }
