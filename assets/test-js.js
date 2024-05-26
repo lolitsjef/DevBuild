0d8433ceed322df17927d60f25c2fb9e558374fa
lolitsjef <lolitsjef@gmail.com>
2024-05-25 19:51:52 -0600

    test

--- assets/test-js.js
+++ assets/test-js.js
@@ -47,19 +47,20 @@ function checkout(){
          'quantity': 1
          }]
        };
-       fetch(window.Shopify.routes.root + 'cart/add.js', {
-         method: 'POST',
-         headers: {
-           'Content-Type': 'application/json'
-         },
-         body: JSON.stringify(formData)
-       })
-       .then(response => {
-         return response.json();
-       })
-       .catch((error) => {
-         console.error('Error:', error);
-       });
+    console.log(formData);
+    fetch(window.Shopify.routes.root + 'cart/add.js', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
 
 
